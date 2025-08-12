01de25d19293151b6affcc92caec3d04965450cd
ROHIT KUMAR DAS <65764852+rohitdas306@users.noreply.github.com>
2025-08-12 19:49:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int factorial(int n) {
+        // code here
+        if(n==0||n==1){
+            return 1;
+        }
+         return n*factorial(n-1);
+    }
+   
+}
